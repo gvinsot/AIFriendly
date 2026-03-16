6c6aeb557a5f30336772192fa2eb2bc24b0c49ad
Gigi <gvinsot@hotmail.com>
2026-03-16 13:59:14 +0100

    fix matomo

--- webapp/next.config.js
+++ webapp/next.config.js
@@ -12,11 +12,11 @@ const nextConfig = {
             key: "Content-Security-Policy",
             value: [
               "default-src 'self'",
-              "script-src 'self' 'unsafe-inline' 'unsafe-eval'",
+              "script-src 'self' 'unsafe-inline' 'unsafe-eval' *.methodinfo.fr",
               "style-src 'self' 'unsafe-inline'",
               "img-src 'self' data: blob: https:",
               "font-src 'self' data:",
-              "connect-src 'self' https:",
+              "connect-src 'self' https: *.methodinfo.fr",
               "frame-ancestors 'none'",
               "base-uri 'self'",
               "form-action 'self'",
